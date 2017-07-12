70be532ce5aa3637922c10158aa376baf0cbfad2
firstz <firstz@sfu.ca>
2017-07-11 18:05:01 -0700

    fix missing configjs

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(morgan('combined')) // TODO: configure later
 
 // db setup
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongodb, { useMongoClient: !!1 })
+mongoose.connect(config.db, { useMongoClient: !!1 })
 mongoose.connection.on('error', () => {
   log.fatal('unable to connect to mongodb')
 })
