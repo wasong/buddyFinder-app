12846029947a044a02cc24305a7678b92fec9d80
Zhao <firstz@sfu.ca>
2017-07-12 16:44:01 -0700

    added userbyid endpoint

---
+++ server/src/users/store.js
@@ -0,0 +1,25 @@
+const getUserById = id => ({
+  id: '123',
+  email: 'firstz@sfu.ca',
+  age: 23,
+  name: 'Stanley',
+  friends: [],
+  friendsRequested: [],
+  friendsPending: [],
+  gender: 'male',
+  avatar: '',
+})
+
+const getUserByEmail = email => ({
+  id: '123',
+  email: 'firstz@sfu.ca',
+  age: 23,
+  name: 'Stanley',
+  friends: [],
+  friendsRequested: [],
+  friendsPending: [],
+  gender: 'male',
+  avatar: '',
+})
+
+export default { getUserById, getUserByEmail }
