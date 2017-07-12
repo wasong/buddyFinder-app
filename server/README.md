8fce60f6e5c0742aae7f485a4ac812b8d89bae0f
Zhao <firstz@sfu.ca>
2017-07-12 15:42:49 -0700

    restructuring the repos

---
+++ server/README.md
@@ -0,0 +1,8 @@
+# BuddyFinder-api
+Backend service for our BuddyFinder-app
+
+## To start:
+
+`npm install`
+
+`npm run start`
--- src/schema.js
+++
@@ -1,9 +0,0 @@
-import { makeExecutableSchema } from 'graphql-tools'
-
-import resolver from './Resolvers/Resolver'
-import types from './Types/Types'
-
-export default makeExecutableSchema({
-  typeDefs: types,
-  resolvers: resolver,
-})
